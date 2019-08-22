67f6ecc8ce277a75082431634a70b4c1bc97d141
Ilmast Toivo <s1900624@edu.bc.fi>
2019-08-22 10:32:27 +0300

    Add: variant with setInterval()

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 (() => {
     let count = 0;
+    let counterId = 0;
     const counter = () => {
-        setTimeout( () => {
-            console.log('Current count is %s.', count);
+        counterId = setTimeout( () => {
+            console.log('setTimeout(), Current count is %s.', count);
             count = count < 100 ? count += 1 : 0;
             counter();
         }, 120);
     }
     counter();
+})();
+
+
+(() => {
+    let count = 0;
+    setInterval( () => {
+        console.log('setInterval(), Current count is %s.', count);
+        count = count < 100 ? count += 1 : 0;
+    }, 120);
 })();
